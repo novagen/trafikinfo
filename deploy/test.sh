d2d46dafa3da6aad89585339422686701bae87e3
Christofer Engel <christofer.engel@kambi.com>
2018-09-24 13:11:21 +0200

    Another test run fix

---
+++ deploy/test.sh
@@ -0,0 +1,5 @@
+ApiKey=$1
+Referer=$2
+
+xbuild /p:Configuration=Release Trafikinfo.sln
+mono ./testrunner/NUnit.ConsoleRunner.*/tools/nunit3-console.exe ./Trafikverket.Tests/bin/Release/Trafikverket.Tests.dll --params=apiKey=$ApiKey;apiReferer=$Referer
